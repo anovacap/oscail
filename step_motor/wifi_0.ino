f3c2a8744dcc4ba60479f6a982ab8be2351f632a
Hunt6666 <489@holbertonschool.com>
2019-03-16 19:19:09 -0700

    error test

--- step_motor/wifi_0.ino
+++ step_motor/wifi_0.ino
@@ -3,9 +3,12 @@
 const char* ssid     = "drumphsnutz";         // The SSID (name) of the Wi-Fi network you want to connect to
 const char* password = "tinyearth720";     // The password of the Wi-Fi network
 const byte BT2pin = D1;
+const byte BT2test = D4;
 const byte BTpin = D0;
+const byte BTtest =D5;
 int LED = D2;
 int LED2 = D3;
+int ErrorLED = D6;
 int count = 0;
 
 
@@ -29,10 +32,13 @@ void setup() {
   Serial.print("IP address:\t");
   Serial.println(WiFi.localIP());         // Send the IP address of the ESP8266 to the computer
 
+  pinMode(BTtest, INPUT);
+  pinMode(BT2test, INPUT);
   pinMode(BTpin, INPUT);
   pinMode(LED, OUTPUT);
   pinMode(BT2pin, INPUT);
   pinMode(LED2, OUTPUT);
+  pinMode(ErrorLED, OUTPUT);
 }
 
 void loop() {
@@ -42,16 +48,42 @@ void loop() {
  }
  if ( digitalRead(BTpin)==HIGH){ // Tests weather hc5 is connected
    digitalWrite(LED, HIGH);
+   if (digitalRead(BTtest) == LOW) {
+    digitalWrite(ErrorLED, HIGH);
+   }
+   else {
+    digitalWrite(ErrorLED, LOW);
+   }
  }
  else {
    digitalWrite(LED, LOW);
+   if (digitalRead(BTtest) == HIGH) {
+    digitalWrite(ErrorLED, HIGH);
+   }
+   else {
+    digitalWrite(ErrorLED, LOW);
+   }
  }
+
 
  if ( digitalRead(BT2pin)==HIGH){ // Tests weather hc5 is connected
    digitalWrite(LED2, HIGH);
+   if (digitalRead(BT2test) == LOW) {
+    digitalWrite(ErrorLED, HIGH);
+   }
+   else {
+    digitalWrite(ErrorLED, LOW);
+    
+   }
  }
  else {
    digitalWrite(LED2, LOW);
+   if (digitalRead(BTtest == HIGH)) {
+      digitalWrite(ErrorLED, HIGH);
+    }
+    else {
+      digitalWrite(ErrorLED, LOW);
+    }
  }
  count++;
   

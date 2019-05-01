83af4cf43c46a3c3e75964498f40cbfac986296f
Damon Nyhan <damon.nyhan@gmail.com>
2019-05-01 16:03:34 -0700

    Indent code for better readability

--- arduino/arduino_code.ino
+++ arduino/arduino_code.ino
@@ -39,138 +39,136 @@ Serial.println("Hello"); // "Hello" output through serial port
 
 
 void loop() {
-  // look for command in rest mode logic
- digitalWrite(openStatus, HIGH);
- rec = 0;
- if(BTserial.available() > 0) { // checks if connection to bluetooth
-  rec = BTserial.read(); // reads data from bluetooth
-  Serial.print((char)BTserial.read()); //send it to the PC
-  }
+    // look for command in rest mode logic
+    digitalWrite(openStatus, HIGH);
+    rec = 0;
+    if(BTserial.available() > 0) { // checks if connection to bluetooth
+        rec = BTserial.read(); // reads data from bluetooth
+        Serial.print((char)BTserial.read()); //send it to the PC
+    }
 
- if (Serial.available()) {
-  delay(10);
-  fromPC = (char)Serial.read();
-  BTserial.print(fromPC);
-  Serial.print(fromPC);
- }
+    if (Serial.available()) {
+        delay(10);
+        fromPC = (char)Serial.read();
+        BTserial.print(fromPC);
+        Serial.print(fromPC);
+    }
 
- if ( digitalRead(BTpin)==HIGH){ // Tests whether hc05 is connected
-   digitalWrite(toWifi, HIGH);
- }
- else {
-   digitalWrite(toWifi, LOW);
- }
+    if ( digitalRead(BTpin)==HIGH){ // Tests whether hc05 is connected
+        digitalWrite(toWifi, HIGH);
+    }
+    else {
+        digitalWrite(toWifi, LOW);
+    }
 
- if ( digitalRead(BT2pin)==HIGH){ // Tests whether hc05_2 is connected
-   digitalWrite(toWifi2, HIGH);
- }
- else {
-   digitalWrite(toWifi2, LOW);
- }
+    if ( digitalRead(BT2pin)==HIGH){ // Tests whether hc05_2 is connected
+        digitalWrite(toWifi2, HIGH);
+    }
+    else {
+        digitalWrite(toWifi2, LOW);
+    }
 
- if(rec == 'a') {
-  digitalWrite(openStatus, LOW);
-  b = 0;
+    if (rec == 'a') {
+        digitalWrite(openStatus, LOW);
+        b = 0;
 begin_open:
-  rec = 0;
-  for(i = b; i < 600; i++){ //OPEN loop
-     OneStep(true);
-     delay(2);
+    rec = 0;
+    for(i = b; i < 600; i++) { //OPEN loop
+        OneStep(true);
+        delay(2);
   }
 dela: // the delay in door loop befor close after open
-  rec = 0;
-  delay(3000);
-  // Dela postion listen logic
-  if(BTserial.available() > 0) { // checks if connection to bluetooth
-   rec = BTserial.read(); // reads data from bluetooth
-  }
-  if(rec == 'a') {
-    goto dela;
-  }
-  
-  rec = 0;
-  
-  for(a = 600; a > 0; a--){ //CLOSE loop
-     // Closing postion listen loop
+    rec = 0;
+    delay(3000);
+    // Dela postion listen logic
+    if(BTserial.available() > 0) { // checks if connection to bluetooth
+        rec = BTserial.read(); // reads data from bluetooth
+    }
+    if(rec == 'a') {
+        goto dela;
+    }
+    
+    rec = 0;
+    
+    for(a = 600; a > 0; a--){ //CLOSE loop
+        // Closing postion listen loop
 
-   OneStep(false);
-   delay(10);
-   if(BTserial.available() > 0) { // checks if connection to bluetooth
-    rec = BTserial.read(); // reads data from bluetooth
-   }
-   if(rec == 'a') {
-    b = a;
-    goto begin_open;
-   }
-   
-  }
-  digitalWrite(openStatus, HIGH);
-  rec = 0;
- }
+    OneStep(false);
+    delay(10);
+    if(BTserial.available() > 0) { // checks if connection to bluetooth
+        rec = BTserial.read(); // reads data from bluetooth
+    }
+    if(rec == 'a') {
+        b = a;
+        goto begin_open;
+    }
+    
+    }
+    digitalWrite(openStatus, HIGH);
+    rec = 0;
+    }
 
 
 }
 
 // Below code controls the stepper motor
-void OneStep(bool dir){
-    if(dir){
-switch(step_number){
-  case 0:
-  digitalWrite(STEPPER_PIN_1, HIGH);
-  digitalWrite(STEPPER_PIN_2, LOW);
-  digitalWrite(STEPPER_PIN_3, LOW);
-  digitalWrite(STEPPER_PIN_4, LOW);
-  break;
-  case 1:
-  digitalWrite(STEPPER_PIN_1, LOW);
-  digitalWrite(STEPPER_PIN_2, HIGH);
-  digitalWrite(STEPPER_PIN_3, LOW);
-  digitalWrite(STEPPER_PIN_4, LOW);
-  break;
-  case 2:
-  digitalWrite(STEPPER_PIN_1, LOW);
-  digitalWrite(STEPPER_PIN_2, LOW);
-  digitalWrite(STEPPER_PIN_3, HIGH);
-  digitalWrite(STEPPER_PIN_4, LOW);
-  break;
-  case 3:
-  digitalWrite(STEPPER_PIN_1, LOW);
-  digitalWrite(STEPPER_PIN_2, LOW);
-  digitalWrite(STEPPER_PIN_3, LOW);
-  digitalWrite(STEPPER_PIN_4, HIGH);
-  break;
+void OneStep(bool dir) {
+    if(dir) {
+switch(step_number) {
+    case 0:
+    digitalWrite(STEPPER_PIN_1, HIGH);
+    digitalWrite(STEPPER_PIN_2, LOW);
+    digitalWrite(STEPPER_PIN_3, LOW);
+    digitalWrite(STEPPER_PIN_4, LOW);
+    break;
+    case 1:
+    digitalWrite(STEPPER_PIN_1, LOW);
+    digitalWrite(STEPPER_PIN_2, HIGH);
+    digitalWrite(STEPPER_PIN_3, LOW);
+    digitalWrite(STEPPER_PIN_4, LOW);
+    break;
+    case 2:
+    digitalWrite(STEPPER_PIN_1, LOW);
+    digitalWrite(STEPPER_PIN_2, LOW);
+    digitalWrite(STEPPER_PIN_3, HIGH);
+    digitalWrite(STEPPER_PIN_4, LOW);
+    break;
+    case 3:
+    digitalWrite(STEPPER_PIN_1, LOW);
+    digitalWrite(STEPPER_PIN_2, LOW);
+    digitalWrite(STEPPER_PIN_3, LOW);
+    digitalWrite(STEPPER_PIN_4, HIGH);
+    break;
+    }
+    } else {
+        switch(step_number){
+    case 0:
+    digitalWrite(STEPPER_PIN_1, LOW);
+    digitalWrite(STEPPER_PIN_2, LOW);
+    digitalWrite(STEPPER_PIN_3, LOW);
+    digitalWrite(STEPPER_PIN_4, HIGH);
+    break;
+    case 1:
+    digitalWrite(STEPPER_PIN_1, LOW);
+    digitalWrite(STEPPER_PIN_2, LOW);
+    digitalWrite(STEPPER_PIN_3, HIGH);
+    digitalWrite(STEPPER_PIN_4, LOW);
+    break;
+    case 2:
+    digitalWrite(STEPPER_PIN_1, LOW);
+    digitalWrite(STEPPER_PIN_2, HIGH);
+    digitalWrite(STEPPER_PIN_3, LOW);
+    digitalWrite(STEPPER_PIN_4, LOW);
+    break;
+    case 3:
+    digitalWrite(STEPPER_PIN_1, HIGH);
+    digitalWrite(STEPPER_PIN_2, LOW);
+    digitalWrite(STEPPER_PIN_3, LOW);
+    digitalWrite(STEPPER_PIN_4, LOW);
+    }
 }
-  }else{
-    switch(step_number){
-  case 0:
-  digitalWrite(STEPPER_PIN_1, LOW);
-  digitalWrite(STEPPER_PIN_2, LOW);
-  digitalWrite(STEPPER_PIN_3, LOW);
-  digitalWrite(STEPPER_PIN_4, HIGH);
-  break;
-  case 1:
-  digitalWrite(STEPPER_PIN_1, LOW);
-  digitalWrite(STEPPER_PIN_2, LOW);
-  digitalWrite(STEPPER_PIN_3, HIGH);
-  digitalWrite(STEPPER_PIN_4, LOW);
-  break;
-  case 2:
-  digitalWrite(STEPPER_PIN_1, LOW);
-  digitalWrite(STEPPER_PIN_2, HIGH);
-  digitalWrite(STEPPER_PIN_3, LOW);
-  digitalWrite(STEPPER_PIN_4, LOW);
-  break;
-  case 3:
-  digitalWrite(STEPPER_PIN_1, HIGH);
-  digitalWrite(STEPPER_PIN_2, LOW);
-  digitalWrite(STEPPER_PIN_3, LOW);
-  digitalWrite(STEPPER_PIN_4, LOW);
-
-
-}
-  }
 step_number++;
-  if(step_number > 3){
-    step_number = 0;
-  }
+    if(step_number > 3) {
+        step_number = 0;
+    }
 }
